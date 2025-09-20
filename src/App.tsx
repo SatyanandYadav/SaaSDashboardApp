aa93ca9136fc89464dba13c7ce0ce3d101532ec1
Satyanand Yadav <satyanandyadav1517@gmail.com>
2025-09-20 15:48:09 +0530

    prettier + prettier-plugin-tailwindcss setup

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-xl text-blue-500">
+        <p className="flex text-xl text-blue-500">
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
