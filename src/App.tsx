ca6c79a1107ce39ae13c6571062f7df6f496ae60
Satyanand Yadav <satyanandyadav1517@gmail.com>
2025-09-20 15:58:44 +0530

    Added react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import { Link, Navigate, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="flex text-xl text-blue-500">
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <nav className="flex gap-4 bg-gray-100 p-4">
+        <Link to="/dashboard">Dashboard</Link>
+        <Link to="/about">About</Link>
+        <Link to="/home">Home</Link>
+      </nav>
+
+      <Routes>
+        {/* Default route redirects to Dashboard */}
+        <Route path="/" element={<Navigate to="/dashboard" replace />} />
+
+        <Route path="/dashboard" element={<div>Dashboard</div>} />
+        <Route path="/about" element={<div>About</div>} />
+        <Route path="/home" element={<div>Home</div>} />
+
+        {/* Catch-all route redirects to Dashboard */}
+        <Route path="*" element={<Navigate to="/dashboard" replace />} />
+      </Routes>
     </div>
   );
 }
